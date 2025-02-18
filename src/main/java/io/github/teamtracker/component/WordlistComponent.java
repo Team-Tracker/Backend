2437ef98aefe1f0afb5ed9aa5d088e2a6b7eafa8
Lorenz Geyser <lorenz.geyser@outlook.com>
2025-02-18 16:11:08 +0100

    Update

--- src/main/java/io/github/teamtracker/component/WordlistComponent.java
+++ src/main/java/io/github/teamtracker/component/WordlistComponent.java
@@ -1,5 +1,12 @@
 package io.github.teamtracker.component;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +18,8 @@ import jakarta.annotation.PostConstruct;
 @Component
 public class WordlistComponent {
 
+    private static final String RESOURCE_PATH = "src/main/resources/wordlists";
+
     @Autowired
     private WordlistRepository wordlistRepository;
 
@@ -18,6 +27,32 @@ public class WordlistComponent {
     public void init() {
         for (Wordlist wordlistItem : this.wordlistRepository.findAll()) {
             WordFilter.addWord(wordlistItem.getWord());
+        }
+
+        this.read();
+    }
+
+    private void read() {
+        this.readAllFilesLineByLine();
+    }
+
+    public void readAllFilesLineByLine() {
+        try (Stream<Path> paths = Files.walk(Paths.get(RESOURCE_PATH))) {
+            paths.filter(Files::isRegularFile).forEach(this::readFile);
+        } catch (IOException e) {
+            throw new RuntimeException("Error accessing files in resources directory", e);
+        }
+    }
+
+    private void readFile(Path filePath) {
+        System.out.println("Reading file: " + filePath);
+        try (BufferedReader reader = Files.newBufferedReader(filePath)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                WordFilter.addWord(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + filePath + " - " + e.getMessage());
         }
     }
 }
